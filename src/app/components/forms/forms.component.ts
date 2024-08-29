b26d48670f4330b4538acd3aa6e94560ef2eecfe
Renato Moreno Agudo <renatomorenoagudo2@gmail.com>
2024-08-29 19:18:21 -0300

    nao ta rodando

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
+import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from 'src/app/types/location.interface';
 
 @Component({
   selector: 'app-forms',
-  standalone: true,
-  imports: [],
-  templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  templateUrl:'./forms.component.html',
+  styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
-results= [];
+export class FormsComponent implements OnInit {
+  @Output() submitEvent = new EventEmitter();
+  results: Location[] = [];
+  filteredResults: Location[] = [];
+  formGroup!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitsService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService
+    ) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: true
+    })
+    this.unitsService.getAllUnits().subscribe(date=> {
+      this.results = date;
+      this.filteredResults = date;
+    });
+  }
+
+  onSubmit(): void {
+    let { showClosed, hour } = this.formGroup.value
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+    this.unitsService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
+
 }
